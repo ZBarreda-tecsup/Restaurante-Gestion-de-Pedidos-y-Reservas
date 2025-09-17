a7f5ec65e9ff053da2eedfba028f496ea82573b3
robbiejude murillo Gonza <168230819+ryukoark@users.noreply.github.com>
2025-09-17 18:40:57 -0500

    implementaciones

--- CasoSemana5/CasoSemana5/Repository/Interfaces/IRepository.cs
+++ CasoSemana5/CasoSemana5/Repository/Interfaces/IRepository.cs
@@ -1,6 +1,8 @@
-namespace CasoSemana5.Repository.Interfaces;
-
-public interface IRepository
+public interface IRepository<T> where T : class
 {
-    
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(string id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(string id);
 }
---
+++ CasoSemana5/CasoSemana5/Repository/Interfaces/ISystemUserRepository.cs
@@ -0,0 +1,6 @@
+using CasoSemana5.Models;
+
+namespace CasoSemana5.Repository.Interfaces
+{
+    public interface ISystemUserRepository : IRepository<SystemUser> { }
+}
